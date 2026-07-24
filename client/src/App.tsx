0396ac6f0089e7937e88f1409e715a3284b0a489
Nandhini <nandhinikummari537@gmail.com>
2026-07-25 01:42:36 +0530

    commit3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { WizardProvider } from './context/WizardContext';
 import { Navbar } from './components/common/Navbar';
